1d69f982f0f81205c29e60cd1594f02914839f86
az-iB <ouzzabrahim@gmail.com>
2019-04-14 12:18:57 +0100

    Add Item component && Display the data

---
+++ src/components/Item.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<div class="left">
+			<img class="avatar" :src="data.avatr">
+		</div>
+		<div class="right">
+			<span calss="title">
+				{{data.name}}
+			</span>
+			<p class="discription">
+				{{data.description}}
+			</p>
+			<div class="counts">
+				<span class="stars">
+					{{data.nbStars}}
+				</span>
+				<span class="essues">
+					{{data.nbEssues}}
+				</span>
+			</div>
+		</div>
+	</div>
+</template>
+<script >
+	export default {
+		props:['data'],
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
